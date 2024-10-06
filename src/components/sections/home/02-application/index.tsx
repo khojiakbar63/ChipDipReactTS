c428b306f8bf4e3f7f7d293917f446b94b1f672f
bekmuhammad <aripovbekmuhammad2@gmail.com>
2024-10-06 22:32:21 +0500

    heroapp

--- src/components/sections/home/02-application/index.tsx
+++ src/components/sections/home/02-application/index.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { Container, Section } from "../../../../components";
+import { Button, } from '@chakra-ui/react'
 import './_style.scss'
 
 export const Application: React.FC = () => {
   return (
     <Section>
       <Container>
-        <h1>Application</h1>
+        <div className="aplication">
+          <div className="text">
+            <h1>Стать партнером</h1>
+            <p>Приглашаем к сотрудничеству <br />
+              производителей и дистрибьюторов</p>
+          </div>
+
+          <Button colorScheme='blue' w={234} h={42} rounded={20}>Подать заявку</Button>
+
+        </div>
       </Container>
     </Section>
   );
