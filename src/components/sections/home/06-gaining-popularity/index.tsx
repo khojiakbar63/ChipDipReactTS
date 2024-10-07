edf1149a27d8c7f02a0c5737e659e1f57a1f2886
11 <a11@MacBook-11.local>
2024-10-07 13:18:57 +0500

    Home Bestsellers => dev-3

--- src/components/sections/home/06-gaining-popularity/index.tsx
+++ src/components/sections/home/06-gaining-popularity/index.tsx
@@ -1,12 +1,44 @@
 import React from "react";
 import { Container, Section } from "../../../../components";
+import { popularData } from "../../../../mocks";
+import { GoArrowRight, GoArrowLeft } from "react-icons/go";
+
 import './_style.scss'
 
 export const GainingPopularity: React.FC = () => {
   return (
     <Section>
       <Container>
-        <h1>GainingPopularity</h1>
+        <div className="popular-section">
+          <div className="flex">
+            <h2 className="section-title">{popularData.title}</h2>
+            <div className="arrows-button">
+              <button className="arrow-right"><GoArrowLeft /></button>
+              <button className="arrow-left"><GoArrowRight /></button>
+            </div>
+          </div>
+          <div className="cards-container">
+            {popularData.products.filter((_, index) => index !== popularData.products.length - 1).map((product) => (
+              <div key={product.id} className="card">
+                {product.imageUrl && <img src={product.imageUrl} alt={product.name} />}
+                <h3 className="card-brand">{product.brand}</h3>
+                <div className="card-text">
+                  <p className="card-name">{product.name}</p>
+                </div>
+                <div className="card-footer">
+                  {product.price && <p className="card-price">{product.price}</p>}
+                </div>
+              </div>
+            ))}
+            <div className="hits-promo">
+              <div className="promo-text">
+                <h3 className='promo-title'>{popularData.products[popularData.products.length - 1].promoName}</h3>
+                <p className='promo-description'>{popularData.products[popularData.products.length - 1].promoText}</p>
+              </div>
+              <button className="promo-button">{popularData.products[popularData.products.length - 1].buttonText}</button>
+            </div>
+          </div>
+        </div>
       </Container>
     </Section>
   );
---
+++ src/mocks/index.tsx
@@ -0,0 +1,140 @@
+import Card1 from '../assets/images/soeks.png'
+import Card2 from '../assets/images/proskit.png'
+import Card3 from '../assets/images/testo.png'
+import Card4 from '../assets/images/znatok.png'
+import Card5 from '../assets/images/robiton.png'
+import Card6 from '../assets/images/linoya.png'
+import Card7 from '../assets/images/navigator.png'
+import Card8 from '../assets/images/kitay.png'
+import Card9 from '../assets/images/mastech.png'
+import Card10 from '../assets/images/skz.png'
+import Card11 from '../assets/images/diot.png'
+import Card12 from '../assets/images/turbojet.png'
+
+
+
+export const hitsData = {
+    title: "Хиты продаж",
+    products: [
+      {
+        id: 1,
+        brand: "СОЭКС",
+        name: "Эковизор F4, Многофункциональный прибор 4 в 1",
+        price: "14 990 р.",
+        imageUrl: Card1,
+      },
+      {
+        id: 2,
+        brand: "PROSKIT",
+        name: "6PK-301H, Кримпер для обжима изолированных клемм 4.0-6.0",
+        price: "4 350 р.",
+        imageUrl: Card2,
+      },
+      {
+        id: 3,
+        brand: "TESTO",
+        name: "Testo 750-3, Тестер напряжения (Госреестр РФ)",
+        price: "16 500 р.",
+        imageUrl: Card3,
+      },
+      {
+        id: 4,
+        brand: "ЗНАТОК",
+        name: "Конструктор электронный 'Знаток', 180 схем",
+        price: "4 180 р.",
+        imageUrl: Card4,
+      },
+      {
+        id: 5,
+        promoBrand: "PROMO",
+        promoName: "Самые популярные товары",
+        buttonText: "Перейти к выбору",
+        promoText: "Выбор наших покупателей",
+      },
+    ],
+  };
+  
+
+  export const bestOffersData = {
+    title: "Лучшие предложения",
+    products: [
+      {
+        id: 1,
+        brand: "ROBITON",
+        name: "Li16340/3.0, Аккумулятор Li-ion, 550мАч, 3.0В (40.0*37мм)",
+        price: "660 р.",
+        imageUrl: Card5,
+      },
+      {
+        id: 2,
+        brand: "LINQYA",
+        name: "L-UTP4-5 Outdoor, Витая пара, 4 пары Cat5e",
+        price: "64 р.",
+        imageUrl: Card6,
+      },
+      {
+        id: 3,
+        brand: "NAVIGATOR",
+        name: "Элемент питания Navigator 94 762 NBT-NS-6F22-SH1",
+        price: "50 р.",
+        imageUrl: Card7,
+      },
+      {
+        id: 4,
+        brand: "КИТАЙ",
+        name: "ZD-81N, Паяльник-пистолет импульсный, 220В, 30-130Вт",
+        price: "540 р.",
+        imageUrl: Card8,
+      },
+      {
+        id: 5,
+        promoBrand: "",
+        promoName: "Выгодное предложение",
+        buttonText: "Перейти к выбору",
+        promoText: "Узнайте о выгодных предложениях и специальных ценах!",
+      },
+    ],
+  };
+  
+
+  export const popularData = {
+    title: "Набирают популярность",
+    products: [
+      {
+        id: 1,
+        brand: "MASTECH",
+        name: "MS6592P, Измеритель температуры, пирометр",
+        price: "1 350 р.",
+        imageUrl: Card9,
+      },
+      {
+        id: 2,
+        brand: "СЭКС",
+        name: "К78-17Е, 50 мкФ, 450 В (провод +болт), Конденсатор грузовой",
+        price: "510 р.",
+        imageUrl: Card10,
+      },
+      {
+        id: 3,
+        brand: "СИЛОВЫЕ ДИОДЫ",
+        name: "TC142-80-12, Симистор силовой 80А 1200В",
+        price: "2 780 р.",
+        imageUrl: Card11,
+      },
+      {
+        id: 4,
+        brand: "TURBOJET",
+        name: "B1245, Кисточка для флюса в упаковке по 5 шт",
+        price: "400 р.",
+        imageUrl: Card12,
+      },
+      {
+        id: 5,
+        promoBrand: "PROMO",
+        promoName: "Успейте купить первыми",
+        buttonText: "Перейти к выбору",
+        promoText: "Новинки, которые пользуются повышенным спросом",
+      },
+    ],
+  };
+  
